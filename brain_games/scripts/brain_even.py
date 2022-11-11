744244b00edfde9135812c7a8582064cc398aca1
NikitaZhiltsov <34608611+NikitaZhiltsov@users.noreply.github.com>
2022-11-11 12:17:59 +0300

    next edition of brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,4 @@
-#пользователю показывается случайное число. И ему нужно ответить yes, если число чётное, 
-#или no — если нечётное:
+#пользователю показывается случайное число. И ему нужно ответить yes, если число чётное, или no — если нечётное:
 #Welcome to the Brain Games!
 #May I have your name? Bill
 #Hello, Bill!
@@ -32,27 +31,24 @@
 #Congratulations, Sam!
 #Любой некорректный ввод считается ошибкой (например, n) и равносилен неправильному ответу.
 
-#import block
 import prompt
-import random
 from random import randint
 import math
-#from brain_games.cli import welcome_user
 
-
-#создание функции проверка на четность
-def is_eval(num):
-    if num % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
-
-print("Welcome to the Brain Games!")
-name = prompt.string('May I have your name? ')
-print('Hello, ' + name + '!')
 
 def solution():
+    #блок приветствия и условия задачи
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
+    #создаем функцию для проверки четности
+    def is_eval(num):
+        if num % 2 == 0:
+            return 'yes'
+        else:
+            return 'no'
+    #начинаем цикл с условиями - механика игры
     count = 0
     
     while count < 3:
@@ -64,15 +60,19 @@ def solution():
             count+=1
         else:
             if answer == 'no':
-                print(answer + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + name)
+                print("\'" + answer + "\'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + name)
                 return 
             elif answer == 'yes':
-                print(answer + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + name)
+                print("\'" + answer + "\'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + name)
                 return
-            else: 
-                print(answer + "is wrong" + name)
-                return
-            
+            else:
+                if is_eval(num) == 0: 
+                    print("\'" + answer + "\'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + name)
+                    return
+                else:
+                    print("\'" + answer + "\'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + name)
+                    return
+                
     print('Congratulations, ' + name + '!')
 
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from brain_games.cli import welcome_user
-
+#from brain_even import solution
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user(str)
+
+#solution()
 
 
 if __name__ == '__main__':
